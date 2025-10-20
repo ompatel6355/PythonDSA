f99492f412062dc11087ac0779f24995e883e56c
ompatel6355 <omgolakiya@gmail.com>
2025-10-20 12:29:51 -0400

    tuple tutorial commit

---
+++ python 10-16/tuplesTutorial.py
@@ -0,0 +1,18 @@
+fruits1 = ("apple" , "banana", "cherry", "watermelon", "melon")
+fruits2 = ("apple" , "banana", "cherry", "watermelon", "melon")
+# (a, *b, c) = fruits
+# print(b)
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+
+# i = 0
+# while i < len(fruits):
+#     print(fruits[i])
+#     i+=1
+
+
+fruits = fruits1 + fruits2
+print(fruits.count("apple"))
