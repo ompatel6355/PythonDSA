268d9e6106ea017a7503717ef3dc05d490aee701
ompatel6355 <omgolakiya@gmail.com>
2025-11-04 09:33:00 -0500

    function file commit

--- python 10-16/func.py
+++ python 10-16/func.py
@@ -1,4 +1,19 @@
-def greet():
-    print("Hello! Om")
+# def greet():
+#     print("Hello! Om")
 
-greet()
+# greet()
+
+
+
+# def my_function(name, lname):
+#     print(f'Hello {name} {lname}')
+
+# my_function("Om", "patel")
+
+
+def my_funtion():
+    return (10,20)
+
+x,y = my_funtion()
+print('x: ', x)
+print("y: ", y)
