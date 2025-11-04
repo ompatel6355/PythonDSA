33b8bb9e261c8248062f2aaefe86e53ee5a1cbdb
ompatel6355 <omgolakiya@gmail.com>
2025-11-04 13:43:12 -0500

    python tutorial final commit

---
+++ python 10-16/iter.py
@@ -0,0 +1,9 @@
+# mytuple = ("apple", "banana", "cherry")
+
+# for i in mytuple:
+#     print(i)
+
+
+
+import myModule_addition
+myModule_addition.addition(10)
---
+++ python 10-16/myModule_addition.py
@@ -0,0 +1,4 @@
+def addition(n):
+    print(n)
+
+
